8efa148f21155703d996f2449e365f61c1bc524d
thayari <alex.silvanovich@gmail.com>
2020-03-11 18:11:39 +0300

    sse

--- server.js
+++ server.js
@@ -1,20 +1,58 @@
 const http = require('http');
 const Koa = require('koa');
-const koaBody = require('koa-body');
 const Router = require('koa-router');
 const cors = require('koa2-cors');
-const uuidv4 = require('uuid/v4');
+const uuid = require('uuid');
+const moment = require('moment');
 const { streamEvents } = require('http-event-stream');
 
 const app = new Koa();
-const router = new Router({
-  prefix: '/tickets'
-});
+const router = new Router();
 
-app.use(koaBody({
-  urlencoded: true,
-  multipart: true,
-}));
+const msgCount = 50;
+const messages = [];
+
+const actionMsg = {
+  type: 'action',
+  text: 'Идёт перемещение мяча по полю, игроки и той, и другой команды активно пытаются атаковать',
+};
+const freekickMsg = {
+  type: 'freekick',
+  text: 'Нарушение правил, будет штрафной удар',
+};
+const goalMsg = {
+  type: 'goal',
+  text: 'Отличный удар! И Г-О-Л!',
+};
+
+function randomMsg() {
+  const rand = Math.floor(Math.random() * 101);
+  if (rand <= 50) {
+    return actionMsg;
+  } if (rand <= 90) {
+    return freekickMsg;
+  }
+  return goalMsg;
+}
+
+let i = 0;
+const generateMessages = setInterval(() => {
+  if (i < msgCount) {
+    const messageData = randomMsg();
+    moment.locale('ru');
+    messageData.date = moment().format('DD MMMM YYYY, HH:mm:ss');
+    const message = {
+      event: 'message',
+      data: JSON.stringify(messageData),
+      id: uuid.v4(),
+    };
+    messages.push(message);
+    i += 1;
+  } else {
+    clearInterval(generateMessages);
+  }
+}, 3000);
+
 
 app.use(cors({
   origin: '*',
@@ -31,13 +69,20 @@ router.get('/sse', async (ctx) => {
       return [];
     },
     stream(sse) {
+      let j = 0;
       const interval = setInterval(() => {
-        sse.sendEvent({ data: 'hello world' });
-      }, 5000);
+        if (messages.length > j) {
+          sse.sendEvent(messages[j]);
+          j += 1;
+        }
+      }, 4000);
       return () => clearInterval(interval);
-    }
+    },
   });
+
+  ctx.respond = false;
 });
 
 const port = process.env.PORT || 7070;
+// eslint-disable-next-line no-unused-vars
 const server = http.createServer(app.callback()).listen(port);
